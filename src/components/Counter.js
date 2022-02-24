eefddee5fd3e684faa91516012782a953c2853d5
AndreaDemSeferi <61742553+AndreaDemSeferi@users.noreply.github.com>
2022-02-24 22:30:47 +0100

    second

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import classes from './Counter.module.css';
 
@@ -5,13 +6,15 @@ const Counter = () => {
 const dispatch = useDispatch();
 const counter = useSelector(state => state.counter);
 const show = useSelector(state=>state.showCounter);
+console.log(counter);
+
 
 const incrementHandler = () => {
-  dispatch({ type: 'increment',amount:100
+  dispatch({ type: 'increase100',amount:100
   });
 }
 const increaseHandler2 = () => {
-  dispatch({ type: 'increase100',amount:200
+  dispatch({ type: 'increase200',amount:200
   });
 }
 const increaseHandler5 = () => {
@@ -22,9 +25,18 @@ const increaseHandler10 = () => {
   dispatch({ type: 'increase1000',amount:1000
   });
 }
-const decrementHandler = () => {
+const decrementHandler = (pengar) => {
+
+  if (pengar <= 0 || pengar > counter)
+ 
+  {
+    return 
+     console.log(counter);
+  console.log(typeof counter);
+  console.log(pengar + counter);
+  }
   dispatch({
-    type: 'decrement'
+    type: 'decrement',amount:-100
   });
 }
 const decrementHandler2 = () => {
@@ -46,18 +58,45 @@ const decrementHandler10 = () => {
   const toggleCounterHandler = () => {
     dispatch({type:'toggle'});
   };
+  const deposits = (e) => {
+    dispatch(
+      {
+        type:'depositMon',
+      amount:e.target.nextElementSibling.value
+    }
+      );
+
+  };
+  const withdraws = (e) => {
+    dispatch({type:'withdrawMon'})
+    console.log(e.target);
+     
+  };
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{counter}</div>}
      {show && <div>
-        <div><button onClick={incrementHandler}>100</button>
+
+        <div>
+          <div>
+            <button onClick={(deposits)}>Desposit</button>
+          <input type="number" placeholder='deposit'/> 
+         </div>
+       
+          <button onClick={incrementHandler}>100</button>
         <button onClick={increaseHandler2}>200</button>
         <button onClick={increaseHandler5}>500</button>
         <button onClick={increaseHandler10}>1000</button></div>
         <div>
-        <button onClick={decrementHandler}>-100</button>
+
+        <div>
+          <button onClick={(withdraws)}>Withdraw</button>
+          <input type="number" placeholder='withdraw'/> 
+          </div>
+        <button onClick={()=>decrementHandler(-100)}>-100</button>
         <button onClick={decrementHandler2}>-200</button>
         <button onClick={decrementHandler5}>-500</button>
         <button onClick={decrementHandler10}>-1000</button></div>
--- src/store/redux.js
+++ src/store/redux.js
@@ -2,56 +2,72 @@ import {createStore} from'redux';
 
 const initialState = { counter:1000, showCounter:false};
 const counterReducer = (state = initialState, action)=>{
-    if(action.type === 'increment'){
+
+    if(action.type === 'depositMon'){
         return {
-            counter: state.counter +100,
-            showCounter: state.showCounter
+            counter: state.counter + +action.amount,
+            showCounter:state.showCounter
+
 
         };
     }
     if(action.type === 'increase100'){
         return {
             counter: state.counter + action.amount,
-            showCounter: state.showCounter
+            showCounter:state.showCounter
+
+
+        };
+    }
+    if(action.type === 'increase200'){
+        return {
+            counter: state.counter + action.amount,
+            showCounter:state.showCounter
+
 
         };
     }
     if(action.type === 'increase500'){
         return {
             counter: state.counter + action.amount,
-            showCounter: state.showCounter
+          showCounter:state.showCounter
 
         };
     }
     if(action.type === 'increase1000'){
         return {
             counter: state.counter + action.amount,
-            showCounter: state.showCounter
+            showCounter:state.showCounter
+
 
         };
     }
     if(action.type === 'decrement'){
         return {
-            counter: state.counter -100,
-            showCounter: state.showCounter
+            counter: state.counter + action.amount,
+            showCounter:state.showCounter
+
         }
     }
     if(action.type === 'decrement2'){
         return {
             counter: state.counter +action.amount,
-            showCounter: state.showCounter
+            showCounter:state.showCounter
+
         };
     }
     if(action.type === 'decrement5'){
         return {
             counter: state.counter +action.amount,
-            showCounter: state.showCounter
+            showCounter:state.showCounter
+
         };
     }
     if(action.type === 'decrement10'){
         return {
             counter: state.counter +action.amount,
-            showCounter: state.showCounter
+            showCounter:state.showCounter
+
         };
     }
     if(action.type === 'toggle'){
